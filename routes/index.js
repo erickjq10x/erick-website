f17353ad8705c0a03bb150fbdd7a754722398379
erickjq10x <49692048+erickjq10x@users.noreply.github.com>
2021-04-19 13:24:48 -0300

    instalacion
    nada

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+router.get('/productos', function(req,res,next){
+  res.render('productos')
+})
+module.exports = router;
