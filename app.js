93d035faa726dae900890a05abb663f7a9224c19
erickjq10x <49692048+erickjq10x@users.noreply.github.com>
2021-04-20 20:44:52 -0300

    nuevo navbar y agregue el carrusel de mierda
    cambie el nav bar por que el de bootstrap me generaba problemas y estaba para el culo, ahora esta en modo beta mañana intento mejorarlo.
    el carrusel de mierda ya esta puesto esto es por ti silvio chupame los heuvos XD es joda joda prepara el culo que mañana se vienen 25 flexiones por serie .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
