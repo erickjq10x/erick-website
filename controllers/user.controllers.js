c5462a49f01c0af6a21e362f3939aeff122c66e0
erickjq10x <49692048+erickjq10x@users.noreply.github.com>
2021-04-25 11:57:51 -0300

    termine hdp estoy con la pija toda hecha mierda puto
    termine de hacerme la paja y soy feliz.puntos 6 y 7 terminados

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,15 +1,25 @@
+const { InsufficientStorage } = require("http-errors");
 const user = require("../database/schemas/user");
+const bcrypt = require('bcrypt');
 
-exports.render = (req,res)=>{
+exports.render = (req, res) => {
     res.render('register');
 }
 
-exports.registerUser = async (req,values) => {
+exports.registerUser = (req, res) => {
+    console.log('soyputo')
     var values = req.body
-    return newUser = await user.create({
-        fullname: values.name,
-        username: values.username,
-        email: values.email,
-        password: values,password,
+    const saltRounds = 10;
+    const myPlaintextPassword = values.password;
+    bcrypt.hash(myPlaintextPassword, saltRounds, async function (err, hash) {
+        var newUser = await user.create({
+            fullname: values.name,
+            username: values.username,
+            email: values.email,
+            password: hash,
+        });
+        await newUser.save();
     });
+
+    res.render('index')
 }
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 const controller = require('../controllers/user.controllers');
 
 router.get('/', controller.render);
-module.exports = router;
 
 router.post('/new',controller.registerUser);
+
+module.exports = router;
+
